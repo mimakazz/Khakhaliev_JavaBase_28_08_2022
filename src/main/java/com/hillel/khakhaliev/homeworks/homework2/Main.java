ea8a94414baac30c5efa7af4c434d70e7e025dd5
Khakhaliev Kostya <bonlimon124@gmail.com>
2022-09-06 20:30:28 +0200

    init commit

---
+++ src/main/java/com/hillel/khakhaliev/homeworks/homework2/Main.java
@@ -0,0 +1,21 @@
+package com.hillel.khakhaliev.homeworks.homework2;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+       double N = 49.9808;
+       double E = 36.2527;
+       char b1 = '\u00B0';
+
+
+       System.out.println("Wide: "+ N +b1+"N");
+       System.out.println("Length: "+ E +b1+"E");
+
+    }
+
+
+
+
+
+}
